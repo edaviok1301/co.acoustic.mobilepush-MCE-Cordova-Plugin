5b9e450839b5da811f733c5e9c365b7c632d7d12
Emilio Paredes <eparedesba1301@gmail.com>
2021-08-03 13:34:21 -0500

    Update jquery.inview.js

--- js/jquery.inview.js
+++ js/jquery.inview.js
@@ -3,8 +3,8 @@
  *    - based on the idea of Remy Sharp, http://remysharp.com/2009/01/26/element-in-view-event-plugin/
  *    - forked from http://github.com/zuk/jquery.inview/
  */
- alert("se ejecuta el script");
 /**
+
 (function ($) {
   var inviewObjects = {}, viewportSize, viewportOffset,
       d = document, w = window, documentElement = d.documentElement, expando = $.expando, timer;
